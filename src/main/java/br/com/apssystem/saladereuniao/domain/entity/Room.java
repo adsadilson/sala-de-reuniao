f9200f6fdbe13cd31c21795fbc9a365c30ad81a7
adsadilson <adilson.curso@yahoo.com.br>
2021-08-05 18:21:06 -0300

    Crianção da entity ROOM

---
+++ src/main/java/br/com/apssystem/saladereuniao/domain/entity/Room.java
@@ -0,0 +1,37 @@
+package br.com.apssystem.saladereuniao.domain.entity;
+
+import java.time.LocalDate;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+@Entity
+@Table(name = "room")
+@SequenceGenerator(name = "ROOM_ID", sequenceName = "ROOM_ID_SEQ")
+@Data
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+public class Room {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY, generator = "ROOM_ID_SEQ")
+	@EqualsAndHashCode.Include
+	private Long id;
+
+	@Column(length = 100, nullable = false, unique = true)
+	private String name;
+
+	private LocalDate date;
+
+	private String startMour;
+
+	private String endHour;
+
+}
