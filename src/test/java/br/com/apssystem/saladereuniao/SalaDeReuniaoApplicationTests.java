51349bedb3b5f96d7525e05c3124604a0af1a350
adsadilson <adilson.curso@yahoo.com.br>
2021-08-05 17:38:53 -0300

    Inicialização do projeto

---
+++ src/test/java/br/com/apssystem/saladereuniao/SalaDeReuniaoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.apssystem.saladereuniao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SalaDeReuniaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
